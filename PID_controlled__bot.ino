819ad6150cfea77ba3853a30c8c3605075d4023e
Hrishikesh <hrishikeshpable@gmail.com>
2022-06-21 10:01:32 +0530

    serial publishing successful

--- PID_controlled__bot.ino
+++ PID_controlled__bot.ino
@@ -1,76 +1,48 @@
+#define TCP false
 #include "utils.h"
 #include <PID_v1.h>
-//#if TCP
-//#include "ArduinoTcpHardware.h"
-//#else
-//#include "ArduinoHardware.h"
-//#endif
-//
-//
-//#include "geometry_msgs/Twist.h"
-//#include <ros/time.h>
-//#include <tf/tf.h>
-//#include <tf/transform_broadcaster.h>
-//#include <nav_msgs/Odometry.h>
-//#include <geometry_msgs/Vector3.h>
-//
-//
-////initializing handle
-//
-//ros::Nodehandle nh;
-//geometry_msgs::TransformStamped t;
-//nav_msgs::Odometry odom_msg;
-//ros::Publisher odom_pub("odom", &odom_msg);
-//tf::TransformBroadcaster broadcaster;
-//
-////tf variables to be
-//char base_link[] = "/base_link";
-//char odom[] = "/odom";
-//
-////position and velocity variables read from the ODrive
-//
-//long posR;
-//long posL;
-//
-////variables to work out the difference in each cycle
-//
-//long posR_old;
-//long posL_old;
-//long posR_diff;
-//long posL_diff;
-//float posR_mm_diff;
-//float posL_mm_diff;
-//float pos_average_mm_diff;
-//float pos_total_mm; broadcast
-//
-//double x = 0;
-//double y = 0;
-//double theta = 0;
-//
-//
-//char base_link[] = "/base_link";
-//char odom[] = "/odom";
-//
-////position and velocity variables read from the ODrive
-//
-//long posR;
-//long posL;
-//
-////variables to work out the difference in each cycle
-//
-//long posR_old;
-//long posL_old;
-//long posR_diff;
-//long posL_diff;
-//float posR_mm_diff;
-//float posL_mm_diff;
-//float pos_average_mm_diff;
-//float pos_total_mm;
-//
+#if TCP
+#include "ArduinoTcpHardware.h"
+#else
+#include "ArduinoHardware.h"
+#endif    
 
 
-double SetRPS_R = 3;
-double SetRPS_L = 3;
+#include <geometry_msgs/Twist.h>
+#include <ros.h>
+#include <ros/time.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Vector3.h>
+
+#if TCP
+
+#include <WiFi.h>
+const char *ssid = "Galaxy M31A78E";
+const char *password = "rutuhrishi4565";
+// Set the rosserial socket server IP address
+IPAddress server(192,168,111,186);
+//Set the rosserial socket server port
+const uint16_t serverPort = 11411;
+
+#endif
+
+//initializing handle
+
+ros::NodeHandle nh;
+
+geometry_msgs::TransformStamped t;
+nav_msgs::Odometry odom_msg;
+ros::Publisher odom_pub("odom", &odom_msg);
+tf::TransformBroadcaster broadcaster;
+
+//tf variables to be
+char base_link[] = "/base_link";
+char odom[] = "/odom";
+
+double SetRPS_R =5;
+double SetRPS_L =5;
 double Kp = 1.2; double Ki = 9.5 ; double Kd = 0;
 // The values of Kp, Ki, Kd for rpm are 1,1,0 (for 100 ms timer)
 double ctrl_speed = 0;
@@ -92,11 +64,53 @@ void setup() {
 
   SetTimer();
 
-  Serial.begin(9600);
+  #if TCP
+
+    Serial.begin(115200);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    //Connect the ESP to the wifi AP
+    WiFi.begin(ssid, password);
+
+    while(WiFi.status() != WL_CONNECTED)
+    {
+      delay(500);
+      Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    // Set the connection to rosserial socket server
+//    nh.getHardware() -> setConnection(server, serverPort);
+
+    //Another way to get IP
+    Serial.print("IP = ");
+    Serial.println(nh.getHardware()->getLocalIP());
+
+  #endif
+
+  #if !TCP
+    nh.getHardware()->setBaud(115200);
+  #endif
+
+  
+  nh.initNode();
+  nh.advertise(odom_pub);
+  nh.subscribe(sub);
+  broadcaster.init(nh);
+
+
+
+
+  // Serial.begin(9600);
 }
 
 void loop() {
-
+    nh.spinOnce();
 
     input_R = rps_R;
     input_L = rps_L;
@@ -107,17 +121,43 @@ void loop() {
     driveMotor(LEFT, output_L, ClkWise);
     driveMotor(RIGHT, output_R, ClkWise);
 
-    
-//
-//    Serial.print("error right: ");
-//    Serial.println((rps_R-SetRPS_R)/SetRPS_R);
+   
 
       Serial.println(distance_travelled);   
 
-//    Serial.print("rpm Right: ");
-//    Serial.println(rpm_R);
-//    Serial.print("rpm Left: ");
-//    Serial.println(rpm_L);
 
+// *** broadcast odom->base_link transform with tf ***
+  
+    geometry_msgs::TransformStamped t;
+
+    t.header.stamp = nh.now();
+    t.header.frame_id = odom;
+    t.child_frame_id = base_link;
+
+    t.transform.translation.x = x_bot;
+    t.transform.translation.y = y_bot;
+    t.transform.translation.z = 0;
+
+    t.transform.rotation = tf::createQuaternionFromYaw(Theta_bot);
+
+    broadcaster.sendTransform(t);
+
+ // *** broadcast odom message ***
+
+    nav_msgs::Odometry odom_msg;
+    odom_msg.header.stamp = nh.now();
+    odom_msg.header.frame_id = odom;
+    odom_msg.pose.pose.position.x = x_bot;
+    odom_msg.pose.pose.position.y = y_bot;
+    odom_msg.pose.pose.position.z = 0.0;
+
+    odom_msg.pose.pose.orientation = tf::createQuaternionFromYaw(Theta_bot);
+
+    odom_msg.child_frame_id = base_link;
+    odom_msg.twist.twist.linear.x = Velocity_bot;
+    odom_msg.twist.twist.linear.y = 0.0;
+    odom_msg.twist.twist.angular.z = Omega_bot;
+
+    odom_pub.publish(&odom_msg);
 
 }
--- utils.h
+++ utils.h
@@ -1,13 +1,14 @@
-#include "esp_system.h"
+//#include "esp_system.h"
 #define A1 26 // Left
-#define B1 27
+#define B_1 27
 #define P1 25
 #define A2 14 // Right
 #define B2 12
 #define P2 13
 
 #define pi 3.1416
-#define DIAMETER 0.069
+#define DIAMETER 0.067
+#define wheel_dist 0.195
 
 #define LEFT 0
 #define RIGHT 1
@@ -31,26 +32,58 @@ double rps_L = 0;
 double counts_per_rev = 206;
 double count_diff_L = 0;
 double count_diff_R = 0;
+double R_revs = 0;
+double L_revs = 0; //The revolutions made in each interval between two consecutive timers for Left wheel
+double R_dist = 0;
+double L_dist = 0; //The distance travelled in each interval between two consecutive timers for Left wheel
+
 double distance_travelled = 0;
+double revs_total_R = 0;
+double revs_total_L = 0;
+double dist_left = 0;
+double dist_right = 0;
+double Theta_bot = 0;
+double Omega_bot = 0;
+double Omega_R = 0;
+double Omega_L =0;
+double Velocity_bot = 0;
+double x_bot = 0;
+double y_bot = 0;
+double phi = 0;
 
-void Count_to_distance();
+void Differential_drive();
 
+
+void velCallback(const geometry_msgs::Twist &vel)
+{
+  SetRPS_R
+}
 
 void IRAM_ATTR onTimer() {   // 100 ms timer
 
  
   Rcounter_cur = counter_R;    
   count_diff_R = Rcounter_cur - Rcounter_prev;
-  double R_revs = count_diff_R/counts_per_rev;
+  R_revs = count_diff_R/counts_per_rev;
   rps_R = R_revs*100;
+  Omega_R = rps_R * pi * DIAMETER;
+  R_dist = R_revs * pi * DIAMETER;
   Rcounter_prev = Rcounter_cur;
   
 
   Lcounter_cur = counter_L;
   count_diff_L = Lcounter_cur - Lcounter_prev;
-  double L_revs = count_diff_L/counts_per_rev;
+  L_revs = count_diff_L/counts_per_rev;
   rps_L = L_revs*100;
+  Omega_L = rps_L * pi * DIAMETER;
+  L_dist = L_revs * pi * DIAMETER;
   Lcounter_prev = Lcounter_cur;
+
+
+
+  Differential_drive();
+
+
 
 }
 
@@ -65,8 +98,6 @@ void updateCounter_R(){
     counter_R--;
   }
 
-  Count_to_distance();
-
 }
 
 void updateCounter_L(){
@@ -79,15 +110,13 @@ void updateCounter_L(){
   {
     counter_L--;
   }
-
-  Count_to_distance();
   
 }
 
 
 void SetPins() {
   pinMode(A1, OUTPUT);
-  pinMode(B1, OUTPUT);
+  pinMode(B_1, OUTPUT);
   pinMode(P1, OUTPUT);
   pinMode(A2, OUTPUT);
   pinMode(B2, OUTPUT);
@@ -107,6 +136,7 @@ void SetTimer() {
   attachInterrupt(digitalPinToInterrupt(outputA_L), updateCounter_L, RISING);
 }
 
+
 void driveMotor(int Location, double speed, int direction) {
   
   if(Location){
@@ -120,20 +150,33 @@ void driveMotor(int Location, double speed, int direction) {
   else{
     double pwm_L = map(speed, 0, 100, 0, 255);
     digitalWrite(A1, !direction);
-    digitalWrite(B1, direction);
+    digitalWrite(B_1, direction);
     analogWrite(P1, pwm_L);
   }
 }
 
-void Count_to_distance()
+void Differential_drive()
 {
-  double revs_total_L = counter_L/counts_per_rev;
-  double revs_total_R = counter_R/counts_per_rev;
+  revs_total_L = counter_L/counts_per_rev;
+  revs_total_R = counter_R/counts_per_rev;
 
-  double dist_left = revs_total_L * pi * DIAMETER;
-  double dist_right = revs_total_R * pi * DIAMETER;
+  dist_left = revs_total_L * pi * DIAMETER;
+  dist_right = revs_total_R * pi * DIAMETER;
 
   distance_travelled = (dist_left + dist_right)/2;
 
-  
+  Omega_bot = DIAMETER * (Omega_R - Omega_L)/(4 * wheel_dist );
+  Velocity_bot = DIAMETER * (Omega_R + Omega_L)/(4 * wheel_dist );
+
+  phi = ((R_dist - L_dist)/wheel_dist);
+
+  double avg_dist = (L_dist + R_dist)/2;
+
+  x_bot = x_bot + avg_dist * cos(Theta_bot + phi/2);
+  y_bot = y_bot + avg_dist * sin(Theta_bot + phi/2);
+
+  Theta_bot += phi;
+
+  Theta_bot = fmod(Theta_bot, 2*PI);
+
 }
